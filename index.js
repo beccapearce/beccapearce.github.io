53f2bf7cfea962619a0d38de7d7124191b7f1ab7
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-10-17 15:08:47 +0100

    broke the footer fixed the jquery

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $( "#info_title1" ).click(function() {
+    $( "#description1" ).slideToggle( "slow" );
+  });
+
+    $( "#info_title2" ).click(function() {
+    $( "#description2" ).slideToggle( "slow" );
+  });
+
+    $( "#info_title3" ).click(function() {
+    $( "#description3" ).slideToggle( "slow" );
+  });
+
+});
